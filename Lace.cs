b3d67d82082339a38a57942544f001064f86dcd0
jajo21 <89775394+jajo21@users.noreply.github.com>
2021-11-19 12:28:27 +0100

    Lägger till fler kommentarer

--- Lace.cs
+++ Lace.cs
@@ -2,16 +2,23 @@ using System;
 
 namespace multifabriken_jajo21
 {
+    /*-----------------------------Klass-----------------------------
+    Lace innehåller de delar som behövs för att bygga en snöresorder.
+    ----------------------------------------------------------------- */
     class Lace
     {
         private string color;
         private int length;
-
+        /*--------------------------Konstruktor--------------------------
+        Konstruktor som tar in en string parameter och en int parameter.
+        ----------------------------------------------------------------- */
         public Lace(string color, int length) {
             this.color = color;
             this.length = length;
         }
-
+        /*---------------------------Metod---------------------------
+        Metod som returnerar en string med information om ordern.
+        ------------------------------------------------------------- */
         public string GetLaceString()
         {
             return ($"Färg: {color}, Längd: {length}"); 
